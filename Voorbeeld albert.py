8b7d209054cca0943b5c6591ebf221db3c3f9483
toine <toine.k@live.nl>
2024-01-05 18:19:29 +0100

    'Cleaned up initial environment commit, added explanations'

--- Voorbeeld albert.py
+++ Voorbeeld albert.py
@@ -5,6 +5,14 @@ from urdfenvs.robots.generic_urdf.generic_diff_drive_robot import GenericDiffDri
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
 from mpscenes.obstacles.sphere_obstacle import SphereObstacle
 
+def add_obstacles(env, pos, radius):
+    sphere_obst_dict = {
+        "type": "sphere",
+        'movable': False,
+        "geometry": {"position": pos, "radius": radius},
+    }
+    sphere_obst = SphereObstacle(name=f'obstacle_{pos[0]}_{pos[1]}_{pos[2]}', content_dict=sphere_obst_dict)
+    env.add_obstacle(sphere_obst)
 
 def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
     robots = [
@@ -24,42 +32,35 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
         dt=0.01, robots=robots, render=render
     )
 
-    # Adding a sphere obstacle
-    for i in range(10):
-        pos = [2+0.*i, 0.1*i+2, 1]
-        radius = 0.5
-        sphere_obst_dict = {
-            "type": "sphere",
-            'movable': False,
-            "geometry": {"position": pos, "radius": radius},
-        }
-        sphere_obst = SphereObstacle(name=f'obstacle_{pos[0]}_{pos[1]}_{pos[2]}', content_dict=sphere_obst_dict)
-        env.add_obstacle(sphere_obst)
+    add_obstacles(env, [1.5, 0, 0], 0.5)
 
-    # obstacles = env.get_obstacles()
-    # for i in obstacles:
-    #     print(i)
-    # print(obstacles[i].__dict__)
-    # print(obstacles[i]._config['geometry'])
-    # defining an action
     action = np.zeros(env.n())
-    action[0] = 0.2
-    action[1] = 1
-    action[2] = 0
-    action[3] = 0
-    action[4] = 0
-    action[5] = 0
-    action[6] = 0
-    action[7] = 0
-    action[8] = 0
-    action[9] = 0
-    action[10] = 0
+    action[0] = 1 # forward velocity
+    action[1] = 0 # angular velocity
+    action[2] = 0 # joint 1
+    action[3] = 0 # joint 2
+    action[4] = 0 # joint 3
+    action[5] = 0 # joint 4
+    action[6] = 0 # joint 5
+    action[7] = 0 # joint 6
+    action[8] = 0 # joint 7
+    action[9] = 1 # gripper -> snap deze nog niet helemaal
+    action[10] = 0 # gripper -> snap deze nog niet helemaal
+
+    # initial position
     ob = env.reset(
-        pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
+        pos=np.array([0, 0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
     )
 
+
+    #loop door de steps heen, voer een actie uit met env.step(action)
     for _ in range(n_steps):
         ob, *_ = env.step(action)
+        print(np.round(ob['robot_0']['joint_state']['position'], 1))
+        x = float(np.round(ob['robot_0']['joint_state']['position'], 1)[0])
+        y = float(np.round(ob['robot_0']['joint_state']['position'], 1)[1])
+        angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
+
     env.close()
 
 
