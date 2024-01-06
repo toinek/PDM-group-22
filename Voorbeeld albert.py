8dbfd2a3e33f2fd7fa2c5dfa9e89eeadbfb9246e
Mees van der Lecq <mees.tecq@gmail.com>
2024-01-06 20:57:59 +0100

    Created PID controller

--- Voorbeeld albert.py
+++ Voorbeeld albert.py
@@ -14,6 +14,24 @@ def add_obstacles(env, pos, radius):
     sphere_obst = SphereObstacle(name=f'obstacle_{pos[0]}_{pos[1]}_{pos[2]}', content_dict=sphere_obst_dict)
     env.add_obstacle(sphere_obst)
 
+class PIDcontroller:
+
+    # a pid controller to control the angular velocity of the robot, aiming at the next target
+    def __init__(self, kp, ki, kd, dt):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.dt = dt
+        self.error_sum = 0
+        self.error_prev = 0
+
+    def update(self, error, dt):
+        self.error_sum += error * dt
+        error_diff = (error - self.error_prev) / dt
+        self.error_prev = error
+        return self.kp * error + self.ki * self.error_sum + self.kd * error_diff
+    
+
 def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
     robots = [
         GenericDiffDriveRobot(
@@ -40,7 +58,7 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
         print(f"Obstacle position: {(env.get_obstacles()[obstacle])._content_dict['geometry']}")
 
     action = np.zeros(env.n())
-    action[0] = 1 # forward velocity
+    action[0] = 0 # forward velocity
     action[1] = 0 # angular velocity
     action[2] = 0 # joint 1
     action[3] = 0 # joint 2
@@ -63,7 +81,53 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
         x = np.round(ob['robot_0']['joint_state']['position'], 1)[0]
         y = np.round(ob['robot_0']['joint_state']['position'], 1)[1]
         angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
-        print(f'Albert position: {x}, {y}, angle: {angular}')
+
+        path_points = [(1.0, 1.0), (2.0, 1.0), (2.0, 2.0), (1.0, 2.0), (1.0, 1.0)]
+
+        # Use the pid controller to control the angular velocity to aim at the next point and drive towards it, not looping over every target all the time
+        for target in path_points:
+            reached = False
+            print("next target: ", target)
+            while not reached:
+                x = np.round(ob['robot_0']['joint_state']['position'], 1)[0]
+                y = np.round(ob['robot_0']['joint_state']['position'], 1)[1]
+                angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
+        
+                pid = PIDcontroller(0.5, 0.0, 0.001, 0.01)
+                
+                error = np.arctan2(target[1] - y, target[0] - x) - angular
+                angular_vel = pid.update(error, 0.01)
+                action[1] = angular_vel
+                ob, *_ = env.step(action)
+
+                while error < 0.02:
+                    x = np.round(ob['robot_0']['joint_state']['position'], 1)[0]
+                    y = np.round(ob['robot_0']['joint_state']['position'], 1)[1]
+                    angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
+                    # print("test")
+                    action[1] = 0
+                    action[0] = 0.5
+                    ob, *_ = env.step(action)
+
+                    # move to next target when previous target is reached within 0.1m, taking into account that coordinates can be negative
+                    if abs(target[0]) - abs(x) < 0.1 and abs(target[1]) - abs(y) < 0.2:
+                        action[0] = 0
+                        ob, *_ = env.step(action)
+                        print('target reached')
+                        reached = True
+                        break
+
+                
+
+                
+
+
+            
+                
+            
+
+
+        # print(f'Albert position: {x}, {y}, angle: {angular}')
     env.close()
 
 
