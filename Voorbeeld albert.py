6a82bbc68d623ced7b7096051afceb8fd37dc2d9
toine <toine.k@live.nl>
2024-01-10 19:11:02 +0100

    'Reverted wrong commit for main simulation'

--- Voorbeeld albert.py
+++ Voorbeeld albert.py
@@ -4,7 +4,10 @@ import numpy as np
 from urdfenvs.robots.generic_urdf.generic_diff_drive_robot import GenericDiffDriveRobot
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
 from mpscenes.obstacles.sphere_obstacle import SphereObstacle
-from global_path_planning_3d import RRTStar, Node
+
+from global_path_planning_3d import RRTStar
+from local_path_planning import PIDControllerBase
+from add_obstacles import ObstacleAdder
 
 def add_obstacles(env, pos, radius):
     sphere_obst_dict = {
@@ -21,24 +24,9 @@ def get_robot_config(ob):
     angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
     return [x, y, angular]
 
-class PIDcontroller:
-    # a pid controller to control the angular velocity of the robot, aiming at the next target
-    def __init__(self, kp, ki, kd, dt):
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.dt = dt
-        self.error_sum = 0
-        self.error_prev = 0
+def run_albert(n_steps=100000, render=False, goal=True, obstacles=True):
 
-    def update(self, error, dt):
-        self.error_sum += error * dt
-        error_diff = (error - self.error_prev) / dt
-        self.error_prev = error
-        return self.kp * error + self.ki * self.error_sum + self.kd * error_diff
-    
-
-def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
+    # Create robot and environment
     robots = [
         GenericDiffDriveRobot(
             urdf="albert.urdf",
@@ -96,83 +84,17 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
     # Initialize action
     action = np.zeros(env.n())
 
-    # initial position
-    ob = env.reset(
-        pos=np.array([0, 0, -0.5*np.pi, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
-    )
-    ob = ob[0]
-
-    #loop door de steps heen, voer een actie uit met env.step(action)
+    # Perform n steps and follow the computed path
+    base_control = PIDControllerBase(path_points, 0.5, 0., 0.001, 0.01)
     for _ in range(n_steps):
-        # ob, *_ = env.step(action)
-        
-        x = np.round(ob['robot_0']['joint_state']['position'], 1)[0]
-        y = np.round(ob['robot_0']['joint_state']['position'], 1)[1]
-        angular = np.round(ob['robot_0']['joint_state']['position'], 1)[2]
-
-        # Use the pid controller to control the angular velocity to aim at the next point and drive towards it, not looping over every target all the time
-        for target in path_points:
-            if target[0] != 0:
-                angle_reached = False
-                print("next target: ", target)
-                while not angle_reached:
-                    robot_config = get_robot_config(ob)
-                    x = robot_config[0]
-                    y = robot_config[1]
-                    robot_angle = robot_config[2]
-                    pid = PIDcontroller(1, 0.0, 0.001, 0.01)
-                    desired_angle = np.arctan2(target[1] - y, target[0] - x)
-                    angle_error = desired_angle - robot_angle
-                    angular_vel = pid.update(angle_error, 0.01)
-                    action[0] = 0
-                    action[1] = angular_vel
-                    ob, *_ = env.step(action)
-                    print(f'angle error: {angle_error}')
-                    if abs(angle_error) < 0.04:
-                        angle_reached = True
-                        print("angle reached")
-                        target_reached = False
-                        while not target_reached:
-                            robot_config = get_robot_config(ob)
-                            x = robot_config[0]
-                            y = robot_config[1]
-                            print(x,y)
-                            target_error = np.sqrt((target[0] - x)**2 + (target[1] - y)**2)
-                            action[0] = 0.5
-                            action[1] = 0
-                            ob, *_ = env.step(action)
-                            if target_error < 0.5:
-                                print('target reached')
-                                target_reached = True
-                                break
+        robot_config = get_robot_config(ob)
+        forward_velocity, angular_velocity = base_control.follow_path(robot_config)
+        action[0] = forward_velocity
+        action[1] = 10*angular_velocity
+        print(f'forward velocity: {forward_velocity}, angular velocity: {angular_velocity}')
+        ob, *_ = env.step(action)
 
     env.close()
-
-# class RobotArmController:
-#     def __init__(self, kp, ki, kd, dt):
-#         self.kp = kp
-#         self.ki = ki
-#         self.kd = kd
-#         self.dt = dt
-#         self.error_sum = 0
-#         self.error_prev = 0
-
-#     def update(self, error, dt):
-#         self.error_sum += error * dt
-#         error_diff = (error - self.error_prev) / dt
-#         self.error_prev = error
-#         return self.kp * error + self.ki * self.error_sum + self.kd * error_diff
-
-# def get_robot_arm_config(ob):
-#     # get joint configurations
-
-
-# def move_robot_arm_to_target(env, target_position):
-#     arm_controller = RobotArmController(1.0, 0.0, 0.001, 0.01)
-
-
-    # get the current position of the robot arm
-    
 
 
 if __name__ == "__main__":
